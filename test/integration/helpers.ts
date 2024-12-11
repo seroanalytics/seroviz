39ef5610f688dff92cdaa00134b99325206449be
alex.hill@gmail.com <alex.hill@gmail.com>
2024-12-11 14:44:56 +0000

    fix integration test

--- test/integration/helpers.ts
+++ test/integration/helpers.ts
@@ -6,5 +6,6 @@ export async function getFormData(fileName: string) {
     const file = new Blob([await readFile(filePath)], { type: "text/csv" });
     formData.set("file", file, fileName);
     formData.set("xcol", "day");
+    formData.set("series_type", "surveillance");
     return formData
 }
